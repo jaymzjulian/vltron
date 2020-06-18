02b2dd92e857a93b01d678a257f79c0f19e2ecba
Jaymz Julian <jaymz@artificial-stupidity.net>
2020-06-17 17:48:49 -0700

    fix stupid bug

--- release.sh
+++ release.sh
@@ -1,6 +1,8 @@
 if [ "x$1" = "x" ]; then
   echo "REMEMBER RLEEASE ID"
 fi
+rm -rf vxtron32
+rm vxtron32.zip
 mkdir vxtron32
 python ../objtools/vx32-bundle.py vltron.bas vxtron32/vxtron32.bas      
 sed -i "s/GIT MASTER/$*/g" vxtron32/vxtron32.bas
